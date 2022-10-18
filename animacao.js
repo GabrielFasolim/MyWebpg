8b811a81f58f34d28992f71806f47250bf2ff519
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2022-10-18 17:03:33 -0300

    Mudança de layout

---
+++ animacao.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded',() =>{
+    new TypeIt(".animated", {
+        speed:150,
+        waitUntilVisible: true
+    }).type('', {delay:1000})
+    
+    .go();
+})
+
+
